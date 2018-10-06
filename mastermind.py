dec7ce8bd22d6c47f88316dd198a97362a66c59b
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-10-06 03:00:46 -0300

    Added instructions to initial text display

--- mastermind.py
+++ mastermind.py
@@ -9,6 +9,28 @@ print('repo: https://github.com/BlueKraken/mastermind-game\n')
 
 print('To exit at any point use "ctr + z" and hit enter\n')
 
+print('-------------')
+print('Instructions:')
+print('-------------')
+print(
+  'Mastermind is a code-breaking game. The secret code\n'
+  'is a number with only digits from 0 to 5, like:\n' +
+  #TODO: make configurable dificulty
+  '\n2243\n' + 
+  '\n' +
+  'The length of the code is configurable,\n' +
+  'but note that normally this type of game \n' +
+  'is played with a code of thength 4\n' +
+  '\n' + 
+  'Each time you guess, the program will give you feedback as follows:\n' +
+  '\n'+
+  '1. number of hits: number of digits that are in the correct position\n'+
+  '2. number of concurrencies: number of digits that are part of the code,\n' +
+  '   but are not in the right position\n')
+
+print('press enter to continue')
+input()
+
 print('Before we start, let\'s configure your game:\n')
 
 print('1. Do you want to see the code at the start of the game for testing purposes? (y/n)')
